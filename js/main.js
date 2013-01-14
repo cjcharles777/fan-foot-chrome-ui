5dd53077ae0c22fbfe085dab1539fa28fec5fd82
Cedric <cedric@Cedric-ThinkPad-SL510.(none)>
2013-01-14 17:46:03 -0600

    yea

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
+var id = { "https://api.login.yahoo.com/oauth/v2/get_request_token?client_id=123456789012345&"+
+"redirect_uri=https://abcdefghijklmnopqrstuvwxyzabcdef.chromiumapp.org/provider_cb&response_type=token&scope=user_photos"}
 
+
+var makeSignedRequest = function(ck,cks,encodedurl) {     
+ 
+	var accessor = { consumerSecret: cks, tokenSecret: ""};          
+	var message = { action: encodedurl, method: "GET", parameters: [["oauth_version","1.0"],["oauth_consumer_key",ck]]};
+ 
+	OAuth.setTimestampAndNonce(message);
+	OAuth.SignatureMethod.sign(message, accessor);
+ 
+	var parameterMap = OAuth.getParameterMap(message);
+	var baseStr = OAuth.decodeForm(OAuth.SignatureMethod.getBaseString(message));           
+	var theSig = "";
+ 
+	if (parameterMap.parameters) {
+		for (var item in parameterMap.parameters) {
+			for (var subitem in parameterMap.parameters[item]) {
+				if (parameterMap.parameters[item][subitem] == "oauth_signature") {
+					theSig = parameterMap.parameters[item][1];                    
+					break;                      
+				}
+			}
+		}
+	}
+ 
+	var paramList = baseStr[2][0].split("&");
+	paramList.push("oauth_signature="+ encodeURIComponent(theSig));
+	paramList.sort(function(a,b) {
+		if (a[0] < b[0]) return -1;
+		if (a[0] > b[0]) return 1;
+		if (a[1] < b[1]) return  -1;
+		if (a[1] > b[1]) return 1;
+		return 0;
+	});
+ 
+	var locString = "";
+	for (var x in paramList) {
+		locString += paramList[x] + "&";                
+	}
+ 
+	var finalStr = baseStr[1][0] + "?" + locString.slice(0,locString.length - 1);
+ 
+	return finalStr;
+	};
