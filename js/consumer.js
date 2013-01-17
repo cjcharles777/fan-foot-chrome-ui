d15a6a4e3bb8c9ec9fe1000113b14157ffd7adf7
Cedric <cedric@Cedric-ThinkPad-SL510.(none)>
2013-01-16 18:12:32 -0600

    awesomeness

--- js/consumer.js
+++ js/consumer.js
@@ -15,33 +15,35 @@ consumer.yahoo =
   }
 };
 
-consumer.signForm =
-function signForm(form, etc) {
-    form.action = etc.URL.value;
-    var accessor = { consumerSecret: consumer.yahoo.consumerSecret.value
-                   , tokenSecret   : consumer.yahoo.tokenSecret.value};
+
+function signForm() {
+	var form = {action : "",
+				method : "" };
+    form.action = consumer.yahoo.serviceProvider.requestTokenURL;
+    form.method = "POST";
+    var accessor = { consumerSecret: consumer.yahoo.consumerSecret
+                   , tokenSecret   : consumer.yahoo.tokenSecret};
     var message = { action: form.action
                   , method: form.method
-                  , parameters: []
+                  , parameters: []    
                   };
-    for (var e = 0; e < form.elements.length; ++e) {
-        var input = form.elements[e];
-        if (input.name != null && input.name != "" && input.value != null
-            && (!(input.type == "checkbox" || input.type == "radio") || input.checked))
-        {
-            message.parameters.push([input.name, input.value]);
-        }
-    }
+                  
     OAuth.setTimestampAndNonce(message);
     OAuth.SignatureMethod.sign(message, accessor);
     //alert(outline("message", message));
     var parameterMap = OAuth.getParameterMap(message.parameters);
-    for (var p in parameterMap) {
-        if (p.substring(0, 6) == "oauth_"
-         && form[p] != null && form[p].name != null && form[p].name != "")
-        {
-            form[p].value = parameterMap[p];
-        }
-    }
+   
+    
     return true;
-};
+}
+function main() {
+  // Initialization work goes here.
+}
+function clickHandler(e) {
+  setTimeout(signForm, 1000);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('button').addEventListener('click', clickHandler);
+  main();
+});
