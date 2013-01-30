3feea08f5755a037ce173edc22e821245d5cd7b7
cjcharles777 <cjcharles777@gmail.com>
2013-01-29 22:04:25 -0600

    1/29/2013 - IndexedDB up and somewhat running :-) Oauth can finally progress.

--- js/db/oauthdb.js
+++ js/db/oauthdb.js
@@ -1,6 +1,50 @@
-db = null;
-
+//(function () {
+  var COMPAT_ENVS = [
+    ['Firefox', ">= 16.0"],
+    ['Google Chrome',
+     ">= 24.0 (you may need to get Google Chrome Canary), NO Blob storage support"]
+  ];
+  var compat = $('#compat');
+  compat.empty();
+  compat.append('<ul id="compat-list"></ul>');
+  COMPAT_ENVS.forEach(function(val, idx, array) {
+    $('#compat-list').append('<li>' + val[0] + ': ' + val[1] + '</li>');
+  });
+ 
+  const DB_NAME = 'fantasyfootballdb';
+  const DB_VERSION = 1; // Use a long long for this value (don't use a float)
+  const DB_STORE_NAME = 'oauth';
+ 
+  var db;
+ 
+  // Used to keep track of which view is displayed to avoid to uselessly reload it
+  var current_view_pub_key;
+ 
 function openDb() {
+	
+    console.log("openDb ...");
+    var req = indexedDB.open(DB_NAME, DB_VERSION);
+    req.onsuccess = function (evt) {
+      // Better use "this" than "req" to get the result to avoid problems with
+      // garbage collection.
+      // db = req.result;
+      db = this.result;
+      console.log("openDb DONE");
+    };
+    req.onerror = function (evt) {
+      console.error("openDb:", evt.target.errorCode);
+    };
+ 
+    req.onupgradeneeded = function (evt) {
+      console.log("openDb.onupgradeneeded");
+      var store = evt.currentTarget.result.createObjectStore(
+        DB_STORE_NAME, { keyPath: 'id', autoIncrement: true });
+ 
+      //store.createIndex('biblioid', 'biblioid', { unique: true });
+      store.createIndex('oauth_token', 'oauth_token', { unique: true });
+      //store.createIndex('year', 'year', { unique: false });
+    };	
+/**
   var request = indexedDB.open("fantasyfootballdb");
 
   request.onsuccess = function(e) 
@@ -23,7 +67,6 @@ function openDb() {
         };
       };
     
-
-
-  
-};
+**/  
+}
+//})(); // Immediately-Invoked Function Expression (IIFE)
